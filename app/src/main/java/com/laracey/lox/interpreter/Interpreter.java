c3450356ba3265f8a2f31b23a359148c4d448c18
Kyle Laracey <kalaracey@gmail.com>
2024-11-21 13:25:06 -0500

    11. Resolving and Binding
    At the beginning of this chapter, `examples/11.1-static-scope-closure.lox` did not work: the second call to `showA()` printed "block" rather than "global". Now it prints "global" as expected.

--- app/src/main/java/com/laracey/lox/interpreter/Interpreter.java
+++ app/src/main/java/com/laracey/lox/interpreter/Interpreter.java
@@ -14,11 +14,14 @@ import com.laracey.lox.interpreter.Stmt.Print;
 import com.laracey.lox.interpreter.Stmt.Var;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   final Environment globals = new Environment();
   private Environment environment = globals;
+  private final Map<Expr, Integer> locals = new HashMap<>();
 
   Interpreter() {
     globals.define(
@@ -170,6 +173,10 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     stmt.accept(this);
   }
 
+  void resolve(Expr expr, int depth) {
+    locals.put(expr, depth);
+  }
+
   void executeBlock(List<Stmt> statements, Environment environment) {
     Environment previous = this.environment;
     try {
@@ -249,13 +256,30 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   @Override
   public Object visitAssignExpr(Assign expr) {
     Object value = evaluate(expr.value);
-    environment.assign(expr.name, value);
+
+    Integer distance = locals.get(expr);
+    if (distance != null) {
+      environment.assignAt(distance, expr.name, value);
+    } else {
+      globals.assign(expr.name, value);
+    }
+
     return value;
   }
 
   @Override
   public Object visitVariableExpr(Variable expr) {
-    return environment.get(expr.name);
+    // return environment.get(expr.name);
+    return lookUpVariable(expr.name, expr);
+  }
+
+  private Object lookUpVariable(Token name, Expr expr) {
+    Integer distance = locals.get(expr);
+    if (distance != null) {
+      return environment.getAt(distance, name.lexeme);
+    } else {
+      return globals.get(name);
+    }
   }
 
   private boolean isTruthy(Object object) {
